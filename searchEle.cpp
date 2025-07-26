1e7caed7959f8589147e28acfd93a2cd036b2f60
Pankaj <ydvpnkj28@gmail.com>
2025-07-26 12:09:32 +0530

    searching element in the array using binary search

--- searchEle.cpp
+++ searchEle.cpp
@@ -11,10 +11,28 @@ int linearSearch(int a[], int size, int k){
     return -1;
 }
 
+int binarySearch(int a[], int size, int k){
+    int s=0;
+    int e=size-1;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(a[mid]<k){
+            s=mid+1;
+        }else if(a[mid]>k){
+            e=mid-1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
 int main(){
     int a[] = {1,3,5,6,8,9};
     int size = sizeof(a)/sizeof(a[0]);
-    int index = linearSearch(a, size, 4);
+    int k = 11; //searching element
+    int index1 = linearSearch(a, size, k);
+    int index = binarySearch(a, size, k);
     if(index>0)
         cout << "Value is present at index : " << index << endl;
     else
