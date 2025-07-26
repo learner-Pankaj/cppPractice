814520e8807b01277207e53fb98393c4a2cee4a4
Pankaj <ydvpnkj28@gmail.com>
2025-07-26 13:46:11 +0530

    search element in the array using binary search taking help of recurssion

--- linearSearchRecur.cpp
+++ linearSearchRecur.cpp
@@ -11,13 +11,33 @@ int linearSearchUsingRecursion(int a[], int n, int k, int i){
     return linearSearchUsingRecursion(a, n, k, i+1);
 }
 
+int binarySearchUsingResursion(int a[], int s, int e, int k){
+    if(s<=e){
+        int mid=(s+e)/2;
+        if(a[mid]==k){
+            return mid;
+        }
+        else if(a[mid]>k){
+            return binarySearchUsingResursion(a,s,mid-1,k);
+        }else{
+            return binarySearchUsingResursion(a,mid+1,e,k);
+        }
+    }
+    return -1;
+}
+
 int main(){
-    int a[] = {1,3,6,8,11};
+    int a[] = {1,3,6,8,11,15};
     int n = sizeof(a)/sizeof(a[0]);
     int k = 8;
-    int index = linearSearchUsingRecursion(a, n, k, 0);
-    if(index>0)
-        cout << "Value is present at index : " << index << endl;
+
+    // int index_linear = linearSearchUsingRecursion(a, n, k, 0);
+    
+    int s=0; int e=n-1;
+    int index_binary = binarySearchUsingResursion(a, s, e, k);
+
+    if(index_binary>0)
+        cout << "Value is present at index : " << index_binary << endl;
     else   
         cout << "Value is not present in the array.";
 
