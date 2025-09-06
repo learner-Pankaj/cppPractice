9204c6a5c7d234fac5a4879370ec3b4435fe9db7
Pankaj <ydvpnkj28@gmail.com>
2025-09-06 21:01:36 +0530

    thread with class example

--- threadWithClass.cpp
+++ threadWithClass.cpp
@@ -16,13 +16,20 @@ class Test{
                 cout << "Hi" << endl;
             }
         }
+
+        static void threadFunc3(){
+            cout << "This is static thread function, no need to pass obj of class.\n";
+        }
 };
 
 int main(){
     Test obj;
-    thread th1(Test::threadFunc1, &obj, 1, 25);
-    thread th2(Test::threadFunc2, &obj, 26, 50);
-    th1.join();
-    th2.join();
+    // thread th1(Test::threadFunc1, &obj, 1, 25);
+    // thread th2(Test::threadFunc2, &obj, 26, 50);
+    thread th3(Test::threadFunc3);
+    // th1.join();
+    // th2.join();
+    th3.join();
+    cout << "Id of the thread :: "<<th3.get_id() << endl;
     return 0;
 }
